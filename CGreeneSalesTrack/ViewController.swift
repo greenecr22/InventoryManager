8cecc2a9f6b6a406cb127a195bd38498a23a2c1c
Christopher Greene <greenecr@bc.edu>
2020-04-24 15:37:27 -0400

    Initial Commit

---
+++ CGreeneSalesTrack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CGreeneSalesTrack
+//
+//  Created by Christopher Greene on 4/24/20.
+//  Copyright © 2020 Christopher Greene. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
